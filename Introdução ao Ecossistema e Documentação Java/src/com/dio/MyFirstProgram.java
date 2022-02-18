97830d5175b9984614923e2c3f634212329c7f8d
jaovctr <jvejv2011@gmail.com>
2022-02-17 21:18:32 -0300

    Comentários e laços de repetições

--- Introdução ao Ecossistema e Documentação Java/src/com/dio/MyFirstProgram.java
+++ Introdução ao Ecossistema e Documentação Java/src/com/dio/MyFirstProgram.java
@@ -2,20 +2,41 @@ package com.dio;
 
 import com.dio.base.Order;
 
+/**
+ * @author jaovctr
+ * @version 1.3
+ * @since 1.0
+ * @see Order
+ */
+
 public class MyFirstProgram{
     public static void main(String[] args) {
         //Ordem padrão
-        final Order order = new Order("code1234", 99);
+        String[] items = {"Mouse", "Teclado"};
+        final Order order = new Order("code1234", 99, items);
         double total=order.calculatefee();
-        System.out.println(order + "\n total:" + total );
+        System.out.println(order + "\ntotal:" + total );
+        order.printItemsWhile();
+        System.out.println("\n");
+
         //Ordem com 100
-        final Order order2 = new Order("code5678", 100);
+        String[] items2 = {"Mouse", "Teclado", "Monitor"};
+        final Order order2 = new Order("code5678", 100, items2);
         total=order2.calculatefee();
-        System.out.println(order2 + "\n total:" + total ); 
+        System.out.println(order2 + "\n total:" + total );
+        order2.printItemsDoWhile();
+        System.out.println("\n");
+
         //Ordem com 200
-        final Order order3 = new Order("code9012", 200);
+        String[] items3 = {"Mouse", "Teclado", "Monitor", "Desktop"};
+        final Order order3 = new Order("code9012", 200, items3);
         total=order3.calculatefee();
         System.out.println(order3 + "\n total:" + total );
+        order3.printItemsFor();
+        System.out.println("\n");
+        order3.printItemsEnhancedFor();
+
+        
 
     }
 }
--- Introdução ao Ecossistema e Documentação Java/src/com/dio/base/Order.java
+++ Introdução ao Ecossistema e Documentação Java/src/com/dio/base/Order.java
@@ -1,13 +1,27 @@
 package com.dio.base;
 
+/**
+ * @author jaovctr
+ * @version 1.3
+ * @since 1.0
+ */
 
 public class Order {
     private final String code;
     private final int totalValue;
+    private String[] items; 
 
-    public Order(String code, int totalValue){
+    /**
+     * 
+     * Construtor
+     * @param code código do pedido
+     * @param totalValue valor do pedido
+     * @param items itens do pedido
+     */
+    public Order(String code, int totalValue, String[] items){
         this.code=code;
         this.totalValue=totalValue;
+        this.items=items;
     }
 
     @Override
@@ -16,7 +30,13 @@ public class Order {
 
     }
 
-
+    /**
+     * Exemplos de condicionais
+     */
+    /**
+     *  
+     * @return Valor do pedido com o cálculo das taxas
+     */
     public double calculatefee(){
         if (this.totalValue >100 ){
             switch(this.totalValue){
@@ -29,6 +49,42 @@ public class Order {
             }
         }else{
             return this.totalValue;
+        }
+    }
+
+    /**
+     * Exemplos de laço de repetição
+     */
+
+    /**
+     * Imprime os itens da lista
+    */ 
+    
+    public void printItemsWhile(){
+        int i=0;
+        while(i<items.length){
+            System.out.println(items[i]);
+            i++;
+        }
+    }
+
+    public void printItemsDoWhile(){
+        int i=0;
+        do{
+            System.out.println(items[i]);
+            i++;
+        }while(i<items.length);
+    }
+
+    public void printItemsFor(){
+        for(int i=0;i<items.length;i++){
+            System.out.println(items[i]);
+        }
+    }
+
+    public void printItemsEnhancedFor(){
+        for(String i: items){
+            System.out.println(i);
         }
     }
 }
